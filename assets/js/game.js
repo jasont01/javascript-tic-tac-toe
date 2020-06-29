dc959970ef6d8f35476662132c69e65bb8e64bd1
Jason Thompson <jason@jasont.us>
2020-06-28 20:07:32 -0400

    refactored

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,77 +1,83 @@
-let ticTacToe = {
-  board: new Array(9).fill(" "),
-  currentPlayer: "X",
-  init: function () {
-    this.cacheDom();
-    this.bindEvents();
-    this.render();
-  },
-  cacheDom: function () {
-    this.$board = $('.game-board');
-    this.$button = this.$board.find('.box');
-    this.$newgame = $('#new-game');
-    this.$alert = document.getElementById('alert');
-  },
-  bindEvents: function () {
-    this.$button.on('click', this.validateMove.bind(this));
-    this.$newgame.on('click', this.newGame.bind(this));
-  },
-  validateMove: function (event) {
+let ticTacToe = (() => {
+  let board = new Array(9).fill(" ");
+  let currentPlayer = "X";
+  let winning_lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]];
+
+  render();
+
+  //cacheDom
+  let $gameBoard = $('.game-board');
+  let $button = $gameBoard.find('.box');
+  let $newGame = $('#new-game');
+  let alert = document.getElementById('alert');
+
+  //bindEvents
+  $button.on('click', validateMove.bind(this));
+  $newGame.on('click', newGame.bind(this));
+
+  function validateMove(event) {
     let selected = $(event.target).closest('.box');
-    let i = this.$board.find('.box').index(selected);
-    if (this.board[i] == " ") {
-      this.makeMove(i);
-      this.render();
+    let i = $gameBoard.find('.box').index(selected);
+    if (board[i] == " ") {
+      makeMove(i);
+      render();
     } else {
       alert("Invalid Move")
     };
-  },
-  makeMove: function (move) {
-    this.board[move] = this.currentPlayer
-    this.checkForWinner();
-  },
-  checkForWinner: function () {
-    let winning_lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]];
-    if (this.board.every(square => square != " ")) this.tieGame();
-    if (winning_lines.some(line => line.every(i => this.board[i] == this.currentPlayer))) {
-      this.gameOver();
+  };
+
+  function makeMove(move) {
+    board[move] = currentPlayer
+    checkForWinner();
+  };
+
+  function checkForWinner() {
+    if (board.every(square => square != " ")) tieGame();
+    if (winning_lines.some(line => line.every(i => board[i] == currentPlayer))) {
+      gameOver();
     } else {
-      this.changePlayer();
+      changePlayer();
     };
-  },
-  changePlayer: function () {
-    if (this.currentPlayer == "X") {
-      this.currentPlayer = "O"
+  };
+
+  function changePlayer() {
+    if (currentPlayer == "X") {
+      currentPlayer = "O"
     } else {
-      this.currentPlayer = "X"
+      currentPlayer = "X"
     };
-  },
-  tieGame: function () {
-    this.$alert.innerHTML = `Tie!`;
-    this.$alert.classList.remove("hidden");
-  },
-  gameOver: function () {
-    this.$alert.innerHTML = `${this.currentPlayer} wins!`;
-    this.$alert.classList.remove("hidden");
-  },
-  render: function () {
-    this.board.forEach((value, index) => document.getElementById(index).innerHTML = value);
-  },
-  newGame: function () {
-    this.board = new Array(9).fill(" ");
-    this.currentPlayer = "X";
-    this.$alert.classList.add("hidden");
-    this.render();
+  };
+
+  function tieGame() {
+    alert.innerHTML = `Tie!`;
+    alert.classList.remove("hidden");
+  };
+
+  function gameOver() {
+    alert.innerHTML = `${currentPlayer} wins!`;
+    alert.classList.remove("hidden");
+  };
+
+  function render() {
+    board.forEach((value, index) => document.getElementById(index).innerHTML = value);
+  };
+
+  function newGame() {
+    board = new Array(9).fill(" ");
+    currentPlayer = "X";
+    alert.classList.add("hidden");
+    render();
   }
 
-};
+  return { newGame }
+});
 
-var game = ticTacToe.init();
+let game = ticTacToe();
